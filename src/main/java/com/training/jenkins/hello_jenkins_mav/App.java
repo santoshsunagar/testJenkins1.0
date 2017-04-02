61dce7b66a8375af78ba69a8886168516ee01efa
Santosh Sunagar <santoshncet@gmail.com>
2017-04-02 17:10:13 +0530

    test jenkins

---
+++ src/main/java/com/training/jenkins/hello_jenkins_mav/App.java
@@ -0,0 +1,13 @@
+package com.training.jenkins.hello_jenkins_mav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
